79b988e4c664e8b2e7bd1190d66609da8262cca6
Gregory Bouteiller <gregory.bouteiller@niama.re>
2020-03-10 11:32:42 +0400

    upgrade

--- playgrounds/core/src/boot/i18n.ts
+++
@@ -1,6 +0,0 @@
-import { bootI18n } from '@niama/core';
-import { boot } from 'quasar/wrappers';
-
-import messages from '@/@niama/playground/i18n';
-
-export default boot(({ app, Vue }) => bootI18n({ app, messages, Vue }));
--- playgrounds/core/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-const colors = require('./src/css/colors');
-
-module.exports = {
-  prefix: '_',
-  theme: { colors },
-  variants: {},
-  plugins: [],
-};
