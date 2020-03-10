71ce073262082d2687d78aeca1fa6a54cd37db3a
Gregory Bouteiller <gregory.bouteiller@niama.re>
2020-03-10 13:46:45 +0400

    upgrade

--- quasar/ts/src/install.js
+++ quasar/ts/src/install.js
@@ -1,5 +1,8 @@
+const rimraf = require('rimraf');
+
 const updateContent = (api) => {
   const package = require(api.resolve.app('/package.json'));
+  rimraf.sync(api.resolve.app('jsconfig.json'));
   api.render('./templates', { package });
 };
 
