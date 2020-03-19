71669717ad666babee21ba43a66858e9aa4c4258
Gregory Bouteiller <gregory.bouteiller@niama.re>
2020-03-19 22:52:34 +0400

    feat: add webpack pug

---
+++ webpack/pug/src/index.ts
@@ -0,0 +1,121 @@
+import loaderUtils from 'loader-utils';
+import camelCase from 'lodash-es/camelCase';
+import kebabCase from 'lodash-es/kebabCase';
+import upperFirst from 'lodash-es/upperFirst';
+import pug from 'pug';
+import * as webpack from 'webpack';
+
+import * as T from './types';
+
+export default function(this: webpack.loader.LoaderContext, source: string) {
+  const loaderO: T.LoaderO = loaderUtils.getOptions(this);
+
+  const compilerO: T.CompilerO = {
+    filename: this.resourcePath,
+    doctype: 'html',
+    plugins: [{ preLex: (content: string) => preLex(content, loaderO), postLex: (tokens: T.Token[]) => postLex(tokens, loaderO) }],
+    compileDebug: this.debug || false,
+  };
+
+  const template: T.Template = pug.compile(source, compilerO) as T.Template;
+  template.dependencies.forEach((file) => this.addDependency(file));
+  return template(loaderO.data || {});
+}
+
+// PRELEX PLUGIN ===========================================================================================================================
+
+const preLex = (content: string, opts: T.LoaderO): string => {
+  const { separatorE = '-', separatorM = '--' } = opts;
+
+  const reg: Record<T.ClassType, RegExp> = {
+    BEM: /\.@-(\w+)--(\w+)/g,
+    BE: /\.@-(\w+)/g,
+    BM: /\.@(\w+)--(\w+)/g,
+    B: /\.@(\w+)/g,
+    EM: /\.-(\w+)--(\w+)/g,
+    E: /\.-(\w+)/g,
+  };
+
+  const repl: Record<T.ClassType, string> = {
+    BEM: `.BE${separatorE}$1.BE${separatorE}$1${separatorM}$2`,
+    BE: `.BE${separatorE}$1`,
+    BM: `.B${separatorE}$1.B${separatorE}$1${separatorM}$2`,
+    B: `.B${separatorE}$1`,
+    EM: `.E${separatorE}$1.E${separatorE}$1${separatorM}$2`,
+    E: `.E${separatorE}$1`,
+  };
+
+  return Object.keys(reg).reduce((r, k) => r.replace(reg[k], repl[k]), content);
+};
+
+// POSTLEX PLUGIN ==========================================================================================================================
+
+const postLex = (tokens: T.Token[], opts: T.LoaderO): T.Token[] => {
+  // OPTS ==================================================================================================================================
+
+  const { casingB = 'pascal', casingE = 'camel', casingM = 'camel', separatorE = '-', separatorM = '--' } = opts;
+
+  const format = ({ casing, val }: T.FormatP): string =>
+    casing === 'camel' ? camelCase(val) : casing === 'kebab' ? kebabCase(val) : upperFirst(camelCase(val));
+
+  const modifier = ({ casing, vals }): { main: string; val: string } => {
+    const main = format({ casing, val: vals[0] });
+    return { main, val: main + (vals.length > 1 ? ` ${main}${separatorM}${format({ casing: casingM, val: vals[1] })}` : '') };
+  };
+
+  // DEPTHS ================================================================================================================================
+
+  let depths: T.Depths = { component: 0, line: 0 };
+  const depth = (): number => depths.line + depths.component;
+
+  const updateDepths = (token: T.Token) => {
+    if (token.type === 'indent') depths = { line: depths.line + 1, component: 0 };
+    if (token.type === ':') depths.component++;
+    if (token.type === 'outdent') depths = { line: depths.line - 1, component: 0 };
+    if (token.type === 'newline') depths.component = 0;
+  };
+
+  // BLOCKS ================================================================================================================================
+
+  const blocks: string[] = [];
+
+  const block = (): string =>
+    blocks
+      .slice(0, depth())
+      .filter((v) => v !== null)
+      .pop() || '';
+
+  const updateBlocks = (value: string) => (blocks[depth()] = value);
+
+  // BLOCK TOKENS ==========================================================================================================================
+
+  const isTokenB = (token: T.Token): token is T.ClassToken => token.type === 'class' && token.val.startsWith('B_');
+  const tokenB = (token: T.ClassToken) => {
+    const { main, val } = modifier({ casing: casingB, vals: token.val.replace('B_', '').split(separatorM) });
+    updateBlocks(main);
+    return { ...token, val };
+  };
+
+  // BLOCK ELEMENT TOKENS ==================================================================================================================
+
+  const isTokenBE = (token: T.Token): token is T.ClassToken => token.type === 'class' && token.val.startsWith('BE_');
+  const tokenBE = (token: T.ClassToken) => {
+    const { main, val } = modifier({ casing: casingB, vals: token.val.replace('BE_', '').split(separatorM) });
+    updateBlocks(main);
+    const valE = format({ casing: casingE, val: main });
+    return { ...token, val: `${block()}${separatorE}${valE} ${val}` };
+  };
+
+  // ELEMENT TOKENS ========================================================================================================================
+
+  const isTokenE = (token: T.Token): token is T.ClassToken => token.type === 'class' && token.val.startsWith('E_');
+  const tokenE = (token: T.ClassToken) => {
+    const { val } = modifier({ casing: casingE, vals: (block() + separatorE + token.val.replace('E_', '')).split(separatorM) });
+    return { ...token, val };
+  };
+
+  return tokens.map((token) => {
+    updateDepths(token);
+    return isTokenB(token) ? tokenB(token) : isTokenBE(token) ? tokenBE(token) : isTokenE(token) ? tokenE(token) : token;
+  });
+};
---
+++ webpack/pug/src/types.ts
@@ -0,0 +1,278 @@
+import pug from 'pug';
+
+export type Casing = 'camel' | 'kebab' | 'pascal';
+export type ClassType = 'B' | 'BE' | 'BEM' | 'BM' | 'E' | 'EM';
+
+export interface LoaderO {
+  casingB?: Casing;
+  casingE?: Casing;
+  casingM?: Casing;
+  data?: Record<string, any>;
+  separatorE?: string;
+  separatorM?: string;
+}
+
+export interface CompilerO extends pug.Options {
+  plugins: unknown[];
+}
+
+export interface Template extends pug.compileTemplate {
+  dependencies: string[];
+}
+
+export type Depths = { component: number; line: number };
+
+export type FormatP = { casing: Casing; val: string };
+
+export interface Loc {
+  end: Pos;
+  start: Pos;
+}
+
+export interface Pos {
+  column: number;
+  line: number;
+}
+
+export type LexTokenType =
+  | ':'
+  | '&attributes'
+  | 'attribute'
+  | 'block'
+  | 'blockcode'
+  | 'call'
+  | 'case'
+  | 'class'
+  | 'code'
+  | 'comment'
+  | 'default'
+  | 'doctype'
+  | 'dot'
+  | 'each'
+  | 'else-if'
+  | 'else'
+  | 'end-attributes'
+  | 'end-pipeless-text'
+  | 'end-pug-interpolation'
+  | 'eos'
+  | 'extends'
+  | 'filter'
+  | 'id'
+  | 'if'
+  | 'include'
+  | 'indent'
+  | 'interpolated-code'
+  | 'interpolation'
+  | 'mixin-block'
+  | 'mixin'
+  | 'newline'
+  | 'outdent'
+  | 'path'
+  | 'slash'
+  | 'start-attributes'
+  | 'start-pipeless-text'
+  | 'start-pug-interpolation'
+  | 'tag'
+  | 'text-html'
+  | 'text'
+  | 'when'
+  | 'while'
+  | 'yield';
+
+export interface LexToken<Type extends LexTokenType> {
+  type: Type;
+  loc: Loc;
+}
+
+export interface TagToken extends LexToken<'tag'> {
+  val: string;
+}
+
+export type StartAttributesToken = LexToken<'start-attributes'>;
+
+export interface AttributeToken extends LexToken<'attribute'> {
+  name: string;
+  val: string | boolean;
+  mustEscape: boolean;
+}
+
+export type EndAttributesToken = LexToken<'end-attributes'>;
+
+export interface IndentToken extends LexToken<'indent'> {
+  val: number;
+}
+
+export interface ClassToken extends LexToken<'class'> {
+  val: string;
+}
+
+export type OutdentToken = LexToken<'outdent'>;
+
+export type EosToken = LexToken<'eos'>;
+
+export interface CommentToken extends LexToken<'comment'> {
+  val: string;
+  buffer: boolean;
+}
+
+export type NewlineToken = LexToken<'newline'>;
+
+export interface TextToken extends LexToken<'text'> {
+  val: string;
+}
+
+export interface InterpolatedCodeToken extends LexToken<'interpolated-code'> {
+  mustEscape: boolean;
+  buffer: boolean;
+  val: string;
+}
+
+export interface CodeToken extends LexToken<'code'> {
+  val: string;
+  mustEscape: boolean;
+  buffer: boolean;
+}
+
+export interface IdToken extends LexToken<'id'> {
+  val: string;
+}
+
+export type StartPipelessTextToken = LexToken<'start-pipeless-text'>;
+
+export type EndPipelessTextToken = LexToken<'end-pipeless-text'>;
+
+export interface DoctypeToken extends LexToken<'doctype'> {
+  val: string;
+}
+
+export type DotToken = LexToken<'dot'>;
+
+export interface BlockToken extends LexToken<'block'> {
+  val: string;
+  mode: 'replace' | 'prepend' | 'append';
+}
+
+export type ExtendsToken = LexToken<'extends'>;
+
+export interface PathToken extends LexToken<'path'> {
+  val: string;
+}
+
+export type StartPugInterpolationToken = LexToken<'start-pug-interpolation'>;
+
+export type EndPugInterpolationToken = LexToken<'end-pug-interpolation'>;
+
+export interface InterpolationToken extends LexToken<'interpolation'> {
+  val: string;
+}
+
+export type IncludeToken = LexToken<'include'>;
+
+export interface FilterToken extends LexToken<'filter'> {
+  val: string;
+}
+
+export interface CallToken extends LexToken<'call'> {
+  val: string;
+  args: string;
+}
+
+export interface MixinToken extends LexToken<'mixin'> {
+  val: string;
+  args: string | null;
+}
+
+export interface IfToken extends LexToken<'if'> {
+  val: string;
+}
+
+export type MixinBlockToken = LexToken<'mixin-block'>;
+
+export interface ElseToken extends LexToken<'else'> {
+  val: string;
+}
+
+export interface AndAttributesToken extends LexToken<'&attributes'> {
+  val: string;
+}
+
+export interface TextHtmlToken extends LexToken<'text-html'> {
+  val: string;
+}
+
+export interface EachToken extends LexToken<'each'> {
+  val: string;
+  key: string | null;
+  code: string;
+}
+
+export interface WhileToken extends LexToken<'while'> {
+  val: string;
+}
+
+export interface CaseToken extends LexToken<'case'> {
+  val: string;
+}
+
+export interface WhenToken extends LexToken<'when'> {
+  val: string;
+}
+
+export type ColonToken = LexToken<':'>;
+
+export type DefaultToken = LexToken<'default'>;
+
+export interface ElseIfToken extends LexToken<'else-if'> {
+  val: string;
+}
+
+export type BlockcodeToken = LexToken<'blockcode'>;
+
+export type YieldToken = LexToken<'yield'>;
+
+export type SlashToken = LexToken<'slash'>;
+
+export type Token =
+  | AndAttributesToken
+  | AttributeToken
+  | BlockcodeToken
+  | BlockToken
+  | CallToken
+  | CaseToken
+  | ClassToken
+  | CodeToken
+  | ColonToken
+  | CommentToken
+  | DefaultToken
+  | DoctypeToken
+  | DotToken
+  | EachToken
+  | ElseIfToken
+  | ElseToken
+  | EndAttributesToken
+  | EndPipelessTextToken
+  | EndPugInterpolationToken
+  | EosToken
+  | ExtendsToken
+  | FilterToken
+  | IdToken
+  | IfToken
+  | IncludeToken
+  | IndentToken
+  | InterpolatedCodeToken
+  | InterpolationToken
+  | MixinBlockToken
+  | MixinToken
+  | NewlineToken
+  | OutdentToken
+  | PathToken
+  | SlashToken
+  | StartAttributesToken
+  | StartPipelessTextToken
+  | StartPugInterpolationToken
+  | TagToken
+  | TextHtmlToken
+  | TextToken
+  | WhenToken
+  | WhileToken
+  | YieldToken;
