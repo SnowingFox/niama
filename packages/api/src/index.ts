69a89c6c84e3c93335b9b9adfd7a12b3dc1f967a
Gregory Bouteiller <gregory.bouteiller@niama.re>
2020-04-02 16:03:29 +0400

    feat(api): add bili

---
+++ packages/api/src/index.ts
@@ -0,0 +1,14 @@
+export * from './boot';
+export * from './boot.http';
+export * from './boot.local';
+export * from './boot.rest';
+export * from './boot.utils';
+export * from './boot.ws';
+
+export * from './i18n.fr';
+export { fr as apiFr } from './i18n.fr';
+
+export * from './uses';
+
+export * from './utils';
+export { poF as apiPoF, getError as getApiError } from './utils';
