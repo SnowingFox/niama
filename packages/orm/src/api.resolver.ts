a1a306c6adca13636472f2d9f677621e8d6bed1f
Gregory Bouteiller <gregory.bouteiller@niama.re>
2020-04-11 21:24:14 +0400

    fix(quasar/api): rename package to pkg in scope

--- packages/orm/src/api.resolver.ts
+++ packages/orm/src/api.resolver.ts
@@ -8,35 +8,36 @@ const processWhere = <Dto>(where: T.Dict, item: Dto): boolean =>
     return true;
   });
 
-export const getRS = <C extends T.Cfg>({ rp }: { rp: T.Rp<C> }) => {
-  const { L, O } = rp;
-  return {
-    Mutation: {
-      [L.update]: (_, { data, where: { id } }, { cache }: { cache: T.Api.InMemoryCache }) =>
-        cache.writeData({ data, id: `${L.type}:${id}` }),
-    },
-    Query: {
-      [L.readOne]: async (_: any, { where: { id } }: any, { cache }: { cache: T.Api.InMemoryCache }) => {
-        const data: T.Api.QData<C['ObC']['Po'][]> = cache.readQuery({ query: O.readAll() });
-        await new Promise((r) => setTimeout(r, 3000));
-        return data ? data[L.readMany].find((resource) => resource.id === id) || null : null;
-      },
-      [L.readMany]: async (_: any, { first, orderBy, skip, where }: any, { cache }: { cache: T.Api.InMemoryCache }) => {
-        const data: T.Api.QData<C['ObC']['Po'][]> = cache.readQuery({ query: O.readAll() });
-        if (!data || !data[L.readMany]) return [];
-        let result = data[L.readMany];
-        if (where) result = result.filter((item) => processWhere(where, item));
-        if (first || skip) result = result.slice(skip, skip + first);
-        await new Promise((r) => setTimeout(r, 2000));
-        return result;
-      },
-      [L.count]: (_: any, { where }: any, { cache }: { cache: T.Api.InMemoryCache }): number => {
-        const data: T.Api.QData<C['ObC']['Po'][]> = cache.readQuery({ query: O.readAll() });
-        if (!data || !data[L.readMany]) return 0;
-        let result = data[L.readMany];
-        if (where) result = result.filter((item) => processWhere(where, item));
-        return result.length;
-      },
-    },
-  };
+type Ctx = { cache: T.Api.InMemoryCache };
+
+const readOne = <C extends T.Cfg>({ L, O }: T.Rp<C>) => async (_: any, { where: { id } }: any, { cache }: Ctx) => {
+  const data: T.Api.QData<C['ObC']['Po'][]> = cache.readQuery({ query: O.readAll() });
+  await new Promise((r) => setTimeout(r, 3000));
+  return data ? data[L.readMany].find((resource) => resource.id === id) || null : null;
 };
+
+const readMany = <C extends T.Cfg>({ L, O }: T.Rp<C>) => async (_: any, { first, orderBy, skip, where }: any, { cache }: Ctx) => {
+  const data: T.Api.QData<C['ObC']['Po'][]> = cache.readQuery({ query: O.readAll() });
+  if (!data || !data[L.readMany]) return [];
+  let result = data[L.readMany];
+  if (where) result = result.filter((item) => processWhere(where, item));
+  if (first || skip) result = result.slice(skip, skip + first);
+  await new Promise((r) => setTimeout(r, 2000));
+  return result;
+};
+
+const count = <C extends T.Cfg>({ L, O }: T.Rp<C>) => (_: any, { where }: any, { cache }: Ctx): number => {
+  const data: T.Api.QData<C['ObC']['Po'][]> = cache.readQuery({ query: O.readAll() });
+  if (!data || !data[L.readMany]) return 0;
+  let result = data[L.readMany];
+  if (where) result = result.filter((item) => processWhere(where, item));
+  return result.length;
+};
+
+const update = <C extends T.Cfg>({ L }: T.Rp<C>) => (_, { data, where: { id } }, { cache }: Ctx) =>
+  cache.writeData({ data, id: `${L.type}:${id}` });
+
+export const getRS = <C extends T.Cfg>({ rp }: { rp: T.Rp<C> }) => ({
+  Mutation: { [rp.L.update]: update(rp) },
+  Query: { [rp.L.readOne]: readOne(rp), [rp.L.readMany]: readMany(rp), [rp.L.count]: count(rp) },
+});
