fc627340ab25bd50594d81bfa8179957c40bd993
Gregory Bouteiller <gregory.bouteiller@niama.re>
2020-04-17 11:16:36 +0400

    fix(quasar/ts): remove declaration files

--- quasar/ts/ts/install.ts
+++ quasar/ts/ts/install.ts
@@ -7,5 +7,6 @@ export = async (api: T.InstallAPI) => await updateContent(api);
 const updateContent = async (api: T.InstallAPI) => {
   const pkg = await import(api.resolve.app('/package.json'));
   ['.eslintignore', '.eslintrc.js', 'jsconfig.json', 'tsconfig.json'].forEach((name) => rimraf.sync(api.resolve.app(name)));
+  ['env.d.ts', 'shims.vue.d.ts'].forEach((name) => rimraf.sync(api.resolve.src(name)));
   api.render('./templates', { pkg });
 };
